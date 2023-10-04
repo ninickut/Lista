f6dad38f3c08cc91cb6fe4b908a82dcaa17ecc54
ninickut <120750769+ninickut@users.noreply.github.com>
2023-10-04 00:28:23 -0300

    Update 1.2.c

--- 1.2.c
+++ 1.2.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main (){
-int a = 3, b =4,p;
-p = a;
-a = b;
-b = p;
-printf("%d %d",a,b);
+  int a = 3, b = 4, p;
+  p = a;
+  a = b;
+  b = p;
+  
+  printf("%d %d",a,b);
 }
