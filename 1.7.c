5996b1ca5cba30893c617797ca97565b91a9b07b
ninickut <120750769+ninickut@users.noreply.github.com>
2023-10-04 00:31:51 -0300

    Update 1.7.c

--- 1.7.c
+++ 1.7.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 int main(){
-int r;  
-int a, b, c, d, f;
-printf("Insira o valor em reais:\n");
-scanf("%d",&r);
-a = r / 100;
-b = (r%100)/50;
-c = ((r%100)%50)/10;
-d = (((r%100)%50)%10)/5;
-f = (((r%100)%50)%10)%5;
-printf("%d de 100, %d de 50, %d de 10, %d de 5, %d de 1.",a,b,c,d,f);
+  int r;  
+  int a, b, c, d, f;
+  
+  printf("Insira o valor em reais:\n");
+  scanf("%d",&r);
+  
+  a = r / 100;
+  b = (r%100)/50;
+  c = ((r%100)%50)/10;
+  d = (((r%100)%50)%10)/5;
+  f = (((r%100)%50)%10)%5;
+  
+  printf("%d de 100, %d de 50, %d de 10, %d de 5, %d de 1.",a,b,c,d,f);
 }
 
